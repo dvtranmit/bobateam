de1a2daf9afcf760d4e298ca6dd6a5477c91d73f
Aradhana Adhikari <adhikara@mit.edu>
2017-11-14 21:35:21 -0500

    flash that reads maybe

---
+++ flash_flash_flash/_impact.cmd
@@ -0,0 +1,29 @@
+setMode -bs
+setMode -bs
+setCable -port auto
+Identify 
+identifyMPM 
+assignFile -p 2 -file "/afs/athena.mit.edu/user/a/d/adhikara/Documents/6.111things/flash_flash_flash/lab5.bit"
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+setMode -bs
+deleteDevice -position 1
+deleteDevice -position 1
+deleteDevice -position 1
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
