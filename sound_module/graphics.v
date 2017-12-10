46451adf047f8e60cf11cbb963328dbeb0a013b2
Victoria S Ouyang <vsouyang@mit.edu>
2017-12-10 17:28:18 -0500

    integrated text graphics

--- sound_module/graphics.v
+++ sound_module/graphics.v
@@ -140,77 +140,6 @@ module happymole
 	happy_blue_rom bcm_happy (.clka(pixel_clk), .addra(image_bits), .douta(blue_mapped));	
 endmodule
 
-////////////////////////////////////////////////////////////////////////////////
-//
-// Screens
-//
-////////////////////////////////////////////////////////////////////////////////
-
-module whackamole
-	#(parameter WIDTH = 1020, HEIGHT = 119)
-	(input pixel_clk,
-    input [10:0] x, hcount,
-    input [9:0] y, vcount,
-    output reg [23:0] pixel
-    );
-	wire [16:0] image_addr;
-	wire [3:0] image_bits, red_mapped, green_mapped, blue_mapped;
-	always @ (posedge pixel_clk) begin
-		if ((hcount >= x && hcount < (x + WIDTH)) && (vcount >= y && vcount < (y + HEIGHT)))
-			pixel <= {red_mapped,4'b0, green_mapped,4'b0, blue_mapped,4'b0};
-		else
-			pixel <= 0;
-	end
-	assign image_addr = (hcount - x) + (vcount - y) * WIDTH;
-	whackrom rom1_whack(.clka(pixel_clk),.addra(image_addr),.douta(image_bits));
-	whack_red_rom rcm_whack (.clka(pixel_clk), .addra(image_bits), .douta(red_mapped));
-	whack_green_rom gcm_whack (.clka(pixel_clk), .addra(image_bits), .douta(green_mapped));
-	whack_blue_rom bcm_whack (.clka(pixel_clk), .addra(image_bits), .douta(blue_mapped));	
-endmodule
-
-module startscreen
-	#(parameter WIDTH = 1020, HEIGHT = 119)
-	(input pixel_clk,
-    input [10:0] x, hcount,
-    input [9:0] y, vcount,
-    output reg [23:0] pixel
-    );
-	wire [16:0] image_addr;
-	wire [3:0] image_bits, red_mapped, green_mapped, blue_mapped;
-	always @ (posedge pixel_clk) begin
-		if ((hcount >= x && hcount < (x + WIDTH)) && (vcount >= y && vcount < (y + HEIGHT)))
-			pixel <= {red_mapped,4'b0, green_mapped,4'b0, blue_mapped,4'b0};
-		else
-			pixel <= 0;
-	end
-	assign image_addr = (hcount - x) + (vcount - y) * WIDTH;
-	startrom rom1_start(.clka(pixel_clk),.addra(image_addr),.douta(image_bits));
-	start_red_rom rcm_start (.clka(pixel_clk), .addra(image_bits), .douta(red_mapped));
-	start_green_rom gcm_start (.clka(pixel_clk), .addra(image_bits), .douta(green_mapped));
-	start_blue_rom bcm_start (.clka(pixel_clk), .addra(image_bits), .douta(blue_mapped));	
-endmodule
-
-module gameover
-	#(parameter WIDTH = 1020, HEIGHT = 144)
-	(input pixel_clk,
-    input [10:0] x, hcount,
-    input [9:0] y, vcount,
-    output reg [23:0] pixel
-    );
-	wire [16:0] image_addr;
-	wire [3:0] image_bits, red_mapped, green_mapped, blue_mapped;
-	always @ (posedge pixel_clk) begin
-		if ((hcount >= x && hcount < (x + WIDTH)) && (vcount >= y && vcount < (y + HEIGHT)))
-			pixel <= {red_mapped,4'b0, green_mapped,4'b0, blue_mapped,4'b0};
-		else
-			pixel <= 0;
-	end
-	assign image_addr = (hcount - x) + (vcount - y) * WIDTH;
-	gameoverrom rom1_end(.clka(pixel_clk),.addra(image_addr),.douta(image_bits));
-	gameover_red_rom rcm_end (.clka(pixel_clk), .addra(image_bits), .douta(red_mapped));
-	gameover_green_rom gcm_end (.clka(pixel_clk), .addra(image_bits), .douta(green_mapped));
-	gameover_blue_rom bcm_end (.clka(pixel_clk), .addra(image_bits), .douta(blue_mapped));	
-endmodule
 
 ////////////////////////////////////////////////////////////////////////////////
 //
@@ -222,6 +151,8 @@ module displaymole(	input clk, clk2, reset,
 							input [2:0] mole_location,
 							input [10:0] hcount,
 							input [9:0]  vcount,
+							input [1:0] lives,
+							input [7:0] score,
 							output [23:0] pixel,
 							output reg popup_done = 0);
 
@@ -310,31 +241,159 @@ module displaymole(	input clk, clk2, reset,
 			popup_done <= 0;
 	end
 
-	wire [23:0] startscreen_pixel;
+	wire [23:0] startscreen_pixel, gameoverscreen_pixel, lives_ongoing_pixel, score_ongoing_pixel;
 	wire [23:0] whacktext, amoletext, pressup, gameovertext, livestext, scoretext;
+	wire [23:0] lives0, lives1, lives2, lives3;
 	wire [23:0] d0,d1,d2,d3,d4,d5,d6,d7,d8,d9;
+	wire [23:0] d00,d01,d02,d03,d04,d05,d06,d07,d08,d09;
 	// generate text
 	whack_text_display whack1(.x(60),.hcount(hcount),.y(100),.vcount(vcount),.p_out(whacktext));
 	amole_text_display amole1(.x(476),.hcount(hcount),.y(100),.vcount(vcount),.p_out(amoletext));
 	pressuptostart_text_display pressup1(.x(200),.hcount(hcount),.y(500),.vcount(vcount),.p_out(pressup));
 	gameover_text_display gameover1(.x(150),.hcount(hcount),.y(200),.vcount(vcount),.p_out(gameovertext));
-	lives_text_display lives1(.x(100),.hcount(hcount),.y(200),.vcount(vcount),.p_out(livestext));
-	score_text_display score1(.x(700),.hcount(hcount),.y(200),.vcount(vcount),.p_out(scoretext));
+	lives_text_display lives1(.x(300),.hcount(hcount),.y(300),.vcount(vcount),.p_out(livestext));
+	score_text_display score1(.x(300),.hcount(hcount),.y(400),.vcount(vcount),.p_out(scoretext));
 	
 	assign startscreen_pixel = whacktext | amoletext| pressup;
-	assign gameoverscreen_pixel = gameovertext | scoretext;
-	// generate digits
-	digit0 n0(.x(0),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d0));
-	digit1 n1(.x(100),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d1));
-	digit2 n2(.x(200),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d2));
-	digit3 n3(.x(300),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d3));
-	digit4 n4(.x(400),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d4));
-	digit5 n5(.x(500),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d5));
-	digit6 n6(.x(600),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d6));
-	digit7 n7(.x(700),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d7));
-	digit8 n8(.x(800),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d8));
-	digit9 n9(.x(900),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d9));
+	// generate digits for lives
+	digit0 live0(.x(500),.hcount(hcount),.y(300),.vcount(vcount),.p_out(lives0));
+	digit1 live1(.x(500),.hcount(hcount),.y(300),.vcount(vcount),.p_out(lives1));
+	digit2 live2(.x(500),.hcount(hcount),.y(300),.vcount(vcount),.p_out(lives2));
+	digit3 live3(.x(500),.hcount(hcount),.y(300),.vcount(vcount),.p_out(lives3));
+	// generate 1st digits for score
+	digit0 n0(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d0));
+	digit1 n1(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d1));
+	digit2 n2(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d2));
+	digit3 n3(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d3));
+	digit4 n4(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d4));
+	digit5 n5(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d5));
+	digit6 n6(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d6));
+	digit7 n7(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d7));
+	digit8 n8(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d8));
+	digit9 n9(.x(520),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d9));
+	// generate 2nd digits for score
+	digit0 n0(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d00));
+	digit1 n1(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d01));
+	digit2 n2(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d02));
+	digit3 n3(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d03));
+	digit4 n4(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d04));
+	digit5 n5(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d05));
+	digit6 n6(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d06));
+	digit7 n7(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d07));
+	digit8 n8(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d08));
+	digit9 n9(.x(560),.hcount(hcount),.y(400),.vcount(vcount),.p_out(d09));
 	
+	case(lives)
+		2'd0: lives_ongoing_pixel = livestext | lives0;
+		2'd1: lives_ongoing_pixel = livestext | lives1;
+		2'd2: lives_ongoing_pixel = livestext | lives2;
+		2'd3: lives_ongoing_pixel = livestext | lives3;
+	endcase
+	
+	case(score)
+		8'd0: score_ongoing_pixel = scoretext | d0;
+		8'd1: score_ongoing_pixel = scoretext | d1;
+		8'd2: score_ongoing_pixel = scoretext | d2;
+		8'd3: score_ongoing_pixel = scoretext | d3;
+		8'd4: score_ongoing_pixel = scoretext | d4;
+		8'd5: score_ongoing_pixel = scoretext | d5;
+		8'd6: score_ongoing_pixel = scoretext | d6;
+		8'd7: score_ongoing_pixel = scoretext | d7;
+		8'd8: score_ongoing_pixel = scoretext | d8;
+		8'd9: score_ongoing_pixel = scoretext | d9;
+		8'd10: score_ongoing_pixel = scoretext | d1 | d00;
+		8'd11: score_ongoing_pixel = scoretext | d1 | d01;
+		8'd12: score_ongoing_pixel = scoretext | d1 | d02;
+		8'd13: score_ongoing_pixel = scoretext | d1 | d03;
+		8'd14: score_ongoing_pixel = scoretext | d1 | d04;
+		8'd15: score_ongoing_pixel = scoretext | d1 | d05;
+		8'd16: score_ongoing_pixel = scoretext | d1 | d06;
+		8'd17: score_ongoing_pixel = scoretext | d1 | d07;
+		8'd18: score_ongoing_pixel = scoretext | d1 | d08;
+		8'd19: score_ongoing_pixel = scoretext | d1 | d09;
+		8'd20: score_ongoing_pixel = scoretext | d2 | d00;
+		8'd21: score_ongoing_pixel = scoretext | d2 | d01;
+		8'd22: score_ongoing_pixel = scoretext | d2 | d02;
+		8'd23: score_ongoing_pixel = scoretext | d2 | d03;
+		8'd24: score_ongoing_pixel = scoretext | d2 | d04;
+		8'd25: score_ongoing_pixel = scoretext | d2 | d05;
+		8'd26: score_ongoing_pixel = scoretext | d2 | d06;
+		8'd27: score_ongoing_pixel = scoretext | d2 | d07;
+		8'd28: score_ongoing_pixel = scoretext | d2 | d08;
+		8'd29: score_ongoing_pixel = scoretext | d2 | d09;
+		8'd30: score_ongoing_pixel = scoretext | d3 | d00;
+		8'd31: score_ongoing_pixel = scoretext | d3 | d01;
+		8'd32: score_ongoing_pixel = scoretext | d3 | d02;
+		8'd33: score_ongoing_pixel = scoretext | d3 | d03;
+		8'd34: score_ongoing_pixel = scoretext | d3 | d04;
+		8'd35: score_ongoing_pixel = scoretext | d3 | d05;
+		8'd36: score_ongoing_pixel = scoretext | d3 | d06;
+		8'd37: score_ongoing_pixel = scoretext | d3 | d07;
+		8'd38: score_ongoing_pixel = scoretext | d3 | d08;
+		8'd39: score_ongoing_pixel = scoretext | d3 | d09;
+		8'd40: score_ongoing_pixel = scoretext | d4 | d00;
+		8'd41: score_ongoing_pixel = scoretext | d4 | d01;
+		8'd42: score_ongoing_pixel = scoretext | d4 | d02;
+		8'd43: score_ongoing_pixel = scoretext | d4 | d03;
+		8'd44: score_ongoing_pixel = scoretext | d4 | d04;
+		8'd45: score_ongoing_pixel = scoretext | d4 | d05;
+		8'd46: score_ongoing_pixel = scoretext | d4 | d06;
+		8'd47: score_ongoing_pixel = scoretext | d4 | d07;
+		8'd48: score_ongoing_pixel = scoretext | d4 | d08;
+		8'd49: score_ongoing_pixel = scoretext | d4 | d09;
+		8'd50: score_ongoing_pixel = scoretext | d5 | d00;
+		8'd51: score_ongoing_pixel = scoretext | d5 | d01;
+		8'd52: score_ongoing_pixel = scoretext | d5 | d02;
+		8'd53: score_ongoing_pixel = scoretext | d5 | d03;
+		8'd54: score_ongoing_pixel = scoretext | d5 | d04;
+		8'd55: score_ongoing_pixel = scoretext | d5 | d05;
+		8'd56: score_ongoing_pixel = scoretext | d5 | d06;
+		8'd57: score_ongoing_pixel = scoretext | d5 | d07;
+		8'd58: score_ongoing_pixel = scoretext | d5 | d08;
+		8'd59: score_ongoing_pixel = scoretext | d5 | d09;
+		8'd60: score_ongoing_pixel = scoretext | d6 | d00;
+		8'd61: score_ongoing_pixel = scoretext | d6 | d01;
+		8'd62: score_ongoing_pixel = scoretext | d6 | d02;
+		8'd63: score_ongoing_pixel = scoretext | d6 | d03;
+		8'd64: score_ongoing_pixel = scoretext | d6 | d04;
+		8'd65: score_ongoing_pixel = scoretext | d6 | d05;
+		8'd66: score_ongoing_pixel = scoretext | d6 | d06;
+		8'd67: score_ongoing_pixel = scoretext | d6 | d07;
+		8'd68: score_ongoing_pixel = scoretext | d6 | d08;
+		8'd69: score_ongoing_pixel = scoretext | d6 | d09;
+		8'd70: score_ongoing_pixel = scoretext | d7 | d00;
+		8'd71: score_ongoing_pixel = scoretext | d7 | d01;
+		8'd72: score_ongoing_pixel = scoretext | d7 | d02;
+		8'd73: score_ongoing_pixel = scoretext | d7 | d03;
+		8'd74: score_ongoing_pixel = scoretext | d7 | d04;
+		8'd75: score_ongoing_pixel = scoretext | d7 | d05;
+		8'd76: score_ongoing_pixel = scoretext | d7 | d06;
+		8'd77: score_ongoing_pixel = scoretext | d7 | d07;
+		8'd78: score_ongoing_pixel = scoretext | d7 | d08;
+		8'd79: score_ongoing_pixel = scoretext | d7 | d09;
+		8'd80: score_ongoing_pixel = scoretext | d8 | d00;
+		8'd81: score_ongoing_pixel = scoretext | d8 | d01;
+		8'd82: score_ongoing_pixel = scoretext | d8 | d02;
+		8'd83: score_ongoing_pixel = scoretext | d8 | d03;
+		8'd84: score_ongoing_pixel = scoretext | d8 | d04;
+		8'd85: score_ongoing_pixel = scoretext | d8 | d05;
+		8'd86: score_ongoing_pixel = scoretext | d8 | d06;
+		8'd87: score_ongoing_pixel = scoretext | d8 | d07;
+		8'd88: score_ongoing_pixel = scoretext | d8 | d08;
+		8'd89: score_ongoing_pixel = scoretext | d8 | d09;
+		8'd90: score_ongoing_pixel = scoretext | d9 | d00;
+		8'd91: score_ongoing_pixel = scoretext | d9 | d01;
+		8'd92: score_ongoing_pixel = scoretext | d9 | d02;
+		8'd93: score_ongoing_pixel = scoretext | d9 | d03;
+		8'd94: score_ongoing_pixel = scoretext | d9 | d04;
+		8'd95: score_ongoing_pixel = scoretext | d9 | d05;
+		8'd96: score_ongoing_pixel = scoretext | d9 | d06;
+		8'd97: score_ongoing_pixel = scoretext | d9 | d07;
+		8'd98: score_ongoing_pixel = scoretext | d9 | d08;
+		8'd99: score_ongoing_pixel = scoretext | d9 | d09;
+	endcase
+	
+	assign gameoverscreen_pixel = gameovertext | score_ongoing_pixel;
 	wire [23:0] normal_pixel, dead_pixel, happy_pixel;
 	normalmole #(.WIDTH(212),.HEIGHT(256))
 			normalmole1(.pixel_clk(clk),.x(x),.hcount(hcount),.y(y_change),
@@ -346,10 +405,10 @@ module displaymole(	input clk, clk2, reset,
 			happymole1(.pixel_clk(clk),.x(x),.hcount(hcount),.y(y_change),
 							.y_permanent(y_permanent), .vcount(vcount),.pixel(happy_pixel));
 	assign pixel = state == IDLE ? startscreen_pixel	
-							: (state == MOLE_ASCENDING || state == MOLE_COUNTDOWN) ? normal_pixel 
-							: (state == MOLE_MISSED || state == MOLE_MISSED_SOUND || state == HAPPY_MOLE_DESCENDING) ? happy_pixel
-							: (state == MOLE_WHACKED || state == MOLE_WHACKED_SOUND || state == DEAD_MOLE_DESCENDING) ? dead_pixel
-							: (state == GAME_OVER) ? gameoverscreen_pixel
+							: (state == MOLE_ASCENDING || state == MOLE_COUNTDOWN) ? normal_pixel | lives_ongoing_pixel | score_ongoing_pixel
+							: (state == MOLE_MISSED || state == MOLE_MISSED_SOUND || state == HAPPY_MOLE_DESCENDING) ? happy_pixel | lives_ongoing_pixel | score_ongoing_pixel
+							: (state == MOLE_WHACKED || state == MOLE_WHACKED_SOUND || state == DEAD_MOLE_DESCENDING) ? dead_pixel | lives_ongoing_pixel | score_ongoing_pixel
+							: (state == GAME_OVER) ? gameoverscreen_pixel | score_ongoing_pixel
 							: 24'h0;
 endmodule
 
--- sound_module/happy_green_rom_xmdf.tcl
+++ sound_module/happy_green_rom_xmdf.tcl
@@ -40,6 +40,10 @@ utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path happy_green_
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type asy
 incr fcount
 
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path happy_green_rom.mif
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
 utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path happy_green_rom.ngc
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
 incr fcount
